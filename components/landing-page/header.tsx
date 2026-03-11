57cf9d51187c08addca371ec1902accdaa7012fd
Somanyu Samal <somanyu.03samal@gmail.com>
2026-03-11 22:37:20 +0530

    ui: remove extra components to make it more clean

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -2,36 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { useWorkspaceStore } from "@/store/workspace-store";
-import { NavItem } from "@/types/landing-page";
-import { Menu } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import LogoutButton from "../ui/logout-button";
 
 export default function LandingPageHeader() {
     const router = useRouter()
     const { data: session } = useSession();
     const lastWorkspace = useWorkspaceStore((state) => state.lastVisitedWorkspace)
-
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-
-    const navItems: NavItem[] = [
-        {
-            label: "Product",
-            href: "#",
-        },
-        {
-            label: "Features",
-            href: "#",
-        },
-        {
-            label: "Pricing",
-            href: "#",
-        },
-    ];
 
     let authButtons: React.ReactNode = null
 
@@ -55,7 +34,7 @@ export default function LandingPageHeader() {
         authButtons = (
             <>
                 <Button onClick={handleWorkspaceRedirect}>Workspace</Button>
-                <LogoutButton />
+                <LogoutButton className="lg:block hidden" />
             </>
         )
     } else {
@@ -79,17 +58,6 @@ export default function LandingPageHeader() {
                         <div className="flex items-center gap-x-3">
                             {authButtons}
                         </div>
-                        <Button className="lg:hidden" size="icon" variant="ghost" onClick={() => setIsOpen(!isOpen)}><Menu className="size-7" /></Button>
-                    </div>
-
-                    <div className={`${isOpen ? "block" : "hidden"} justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2">
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            {navItems.map((item) => (
-                                <li key={item.label}>
-                                    <Link href={item.href} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">{item.label}</Link>
-                                </li>
-                            ))}
-                        </ul>
                     </div>
                 </div>
             </nav>
